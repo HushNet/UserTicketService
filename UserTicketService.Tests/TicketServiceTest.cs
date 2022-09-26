89598d640803610ad742ac3ce06335b85bae5a2b
student <student@C256U13>
2022-09-26 18:00:27 +0300

    Добавьте файлы проекта.

---
+++ UserTicketService.Tests/TicketServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UserTicketService.Tests
+{
+    [TestFixture]
+    public class TicketServiceTest
+    {
+        [Test]
+        public void GeTicketPriceMustReturnNotNullableTicket()
+        {
+            var ticketServiceTest = new TicketService();
+            Assert.IsNotNull(ticketServiceTest.GetTicketPrice(1));
+        }
+
+        [Test]
+        public void GeTicketPriceMustThrowException()
+        {
+            var ticketServiceTest = new TicketService();
+            Assert.Throws<TicketNotFoundException>(() => ticketServiceTest.GetTicketPrice(100));
+        }
+
+    }
+}
